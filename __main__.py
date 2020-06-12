a01f0b62b042b1e90256729364f284593b5c7e28
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-06-12 15:45:46 -0400

    basic thing i think

--- __main__.py
+++ __main__.py
@@ -1,7 +1,40 @@
+import sys
+import ctypes
 import pygame
+
+from pygame.locals import *
+
+user32 = ctypes.windll.user32
+screen_size = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+# kekw XD
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+# class Player(pygame.sprite.Sprite):
+#     def __init__(self):
+#         super(Player, self).__init__()
+#         self.surf = pygame.Surface((75, 25))
+#         self.surf.fill((255, 255, 255))
+#         self.rect = self.surf.get_rect()
+py.
+
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode(screen_size)
+# player = Player()
+RUNNING = True
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+while RUNNING:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN: # detect key presses
+            if event.key == K_ESCAPE: # detect esc
+                RUNNING = False # quit
+        if event.type == QUIT: # press quit bitch
+            RUNNING = False # kills stuffs :D
+
+    screen.fill((25, 25, 25))
+    # screen.blit(player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+
+    pygame.display.flip() # display the display to the display
+
+sys.exit()
