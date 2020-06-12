8f6b004875abc2795972da05633e619d611de1b3
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-06-12 17:56:40 -0400

    currently in the process of adding the piece class

--- __main__.py
+++ __main__.py
@@ -13,20 +13,10 @@ SCREEN_HEIGHT = user32.GetSystemMetrics(1)
 pygame.init() # actual game
 
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN) # screen size and stuffs
+pygame.display.set_caption("Caption")
 
 
 RUNNING = True # running variable - will be set to false when x is pressed, quitting the program
-
-
-
-class Piece:
-    def __init__(self, vector):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = pygame.image.load("./assets/red_piece.png")
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.vector = vector
-
 
 
 background_image = pygame.image.load("./assets/table.png")
@@ -34,6 +24,73 @@ white_square = (209, 168, 107)
 black_square = (135, 89, 19)
 square_size = math.floor(SCREEN_HEIGHT / 10)
 SQUARE_DIMENSION = 8
+
+# red down; black up
+
+class Piece(pygame.sprite.Sprite):
+    def __init__(self, team, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        
+        if team == 0:
+            img = "./assets/red_piece.png"
+        elif team == 1:
+            img = "./assets/black_piece.png"
+        else:
+            print("fuck you. you broke everything")
+
+        self.image, self.rect = img, img
+        the_screen = pygame.display.get_surface() # get the screen variable i think
+        self.area = the_screen.get_rect()
+        self.team = team
+        self.x = x
+        self.y = y
+
+    def move_piece(self):
+        self.image = "./assets/glowing_red_piece.png"
+        y_move = self.y - square_size
+
+        if self.team == 1:
+            self.image = "./assets/glowing_black_piece.png"
+            y_move = self.y + square_size
+
+        can_move_left = True
+        can_move_right = True
+        can_jump_left = False
+        can_jump_right = False
+            
+            # Checks if they can move 
+        for loop_piece in pieces:
+            if loop_piece.x == self.x + square_size:
+                can_move_right = False
+                if 
+
+            elif loop_piece == self.x - square_size:
+                can_move_left = False
+
+            
+            #  Draws a grey circle if they can move right/left
+            if can_move_right:
+                pygame.draw.circle(screen, (127, 127, 127), (self.x + square_size, y_move))
+            if can_move_left:
+                pygame.draw.circle(screen, (127, 127, 127), (self.x - square_size, y_move))
+        
+        
+        
+    def render(self):
+        screen.blit(pygame.image.load(self.image), (self.x * square_size, self.y * square_size))
+
+pieces = [Piece(0, 1, 1)]
+def main():
+    print("")
+    for x in range(1, 3):
+        for y in range(1, 8, 2):
+            pieces.append(Piece(0, x, y))
+    for x in range(6, 8):
+        for y in range(1, 8, 2):
+            pieces.append(Piece(0, x, y))
+    
+
+main()
 
 while RUNNING:
     for event in pygame.event.get():
@@ -43,13 +100,11 @@ while RUNNING:
         if event.type == QUIT: # press quit
             RUNNING = False # kills stuffs :D
 
-
-
     screen.blit(background_image, (0, 0))
     for i in range(1, SQUARE_DIMENSION + 1):
         for j in range(1, SQUARE_DIMENSION + 1):
             sqCalc = i
-            if j % 2 == 0:
+            if j % 2 == 1:
                 sqCalc += 1
 
             if sqCalc % 2 == 1:
@@ -58,12 +113,15 @@ while RUNNING:
                 square_colour = black_square
 
             pygame.draw.rect(screen, square_colour, [
-                SCREEN_WIDTH - ((square_size * i) + ((SCREEN_WIDTH - (square_size * 8)) / 2)),
+                math.floor(SCREEN_WIDTH - (square_size * i) - ((SCREEN_WIDTH - (square_size * 8)) / 2)),
                 square_size * j,
                 square_size,
                 square_size
             ])
-            
+
+    for piece in pieces:
+        piece.render()
+
     pygame.display.update()
     pygame.display.flip() # display the display to the display
 
