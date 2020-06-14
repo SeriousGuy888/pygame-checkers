a74fa4b4cd484388cea2d126184b744608c00284
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-06-13 22:40:32 -0400

    i didnt even write this i dont know
    Co-Authored-By: randumperson314 <randumperson314@users.noreply.github.com>
    Co-Authored-By: Markie <markiehoang@users.noreply.github.com>

--- checkers_classes/piece.py
+++ checkers_classes/piece.py
@@ -40,31 +40,29 @@ class Piece(main.pygame.sprite.Sprite):
         # Checks if they can move
         # while checking_jumps: 
         for loop_piece in main.pieces:
-            print(loop_piece.x)
-            print(self.x - 1)
-            print(loop_piece.x == self.x - 1)
+            #  or self.x + 1 > 8
 
             if loop_piece.x == self.x + 1 and loop_piece.y == self.y_move or jump_count > 0: # checks if there is a piece blocking the path
                 can_move_right = False # makes it not able to move if theres a piece blocking the path
                 print("you cant shirt box move right")
-                # if loop_piece.team != self.team: # if the piece that is blocking the path is on the other team, then
-                #     checking_jumps = loop_piece.can_be_jumped("right") # checks if the piece in the path can be jumped
-                #     if checking_jumps: # if it can be jumped, then
-                #         main.pygame.draw.circle(main.screen, ghost_piece, (self.x + 2.5, y_move * 2 + 0.5)) # Creates a gray circle which is an option to move
-                #         jump_count += 1 # Increases jump count 
-                # else: # If the piece is on the same team then
-                #     checking_jumps = False # Doesn't check for jump
+                if loop_piece.team != self.team: # if the piece that is blocking the path is on the other team, then
+                    checking_jumps = loop_piece.can_be_jumped("right") # checks if the piece in the path can be jumped
+                    if checking_jumps: # if it can be jumped, then
+                        main.pygame.draw.circle(main.screen, ghost_piece, (self.x + 2.5, y_move * 2 + 0.5)) # Creates a gray circle which is an option to move
+                        jump_count += 1 # Increases jump count 
+                else: # If the piece is on the same team then
+                    checking_jumps = False # Doesn't check for jump
 
-            if loop_piece == self.x - 1 and loop_piece.y == self.y_move or jump_count > 0:
+            if loop_piece.x == self.x - 1 and loop_piece.y == self.y_move or jump_count > 0:
                 can_move_left = False # Makes it not able to move if theres a piece blocking the path
                 print("ytard cant move left")
-                # if loop_piece.team != self.team: # If the piece that is blocking the path is on the other team, then
-                #     checking_jumps = loop_piece.can_be_jumped("left") # Checks if the piece in the path can be jumped
-                #     if checking_jumps: # If it can be jumped, then
-                #         main.pygame.draw.circle(main.screen, ghost_piece, (self.x - 2.5, y_move * 2 + 0.5)) # Creates a gray circle which is an option to move
-                #         jump_count += 1 # Increases jump count 
-                # else: # If the piece is on the same team then
-                #     checking_jumps = False # Doesn't check for jumps
+                if loop_piece.team != self.team: # If the piece that is blocking the path is on the other team, then
+                    checking_jumps = loop_piece.can_be_jumped("left") # Checks if the piece in the path can be jumped
+                    if checking_jumps: # If it can be jumped, then
+                        main.pygame.draw.circle(main.screen, ghost_piece, (self.x - 2.5, y_move * 2 + 0.5)) # Creates a gray circle which is an option to move
+                        jump_count += 1 # Increases jump count 
+                else: # If the piece is on the same team then
+                    checking_jumps = False # Doesn't check for jumps
                 
         # Draws a ghost piece if they can move right/left
         if can_move_right:
