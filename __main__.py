83eb413162b1ff7cc51a210e27ba2bc66f2b2ee8
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2020-06-14 21:41:43 -0400

    random uselessline removed

--- __main__.py
+++ __main__.py
@@ -106,7 +106,6 @@ def main():
                 RUNNING = False # kills stuffs :D
 
         # pieces[3].move_piece()
-        ghost_pieces
 
         pygame.display.update()
         pygame.display.flip() # display the display to the display
